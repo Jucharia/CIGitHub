8c9d212a9adfa5bd0e98efcd0d98c8a7602c1afa
Yuliya Mylnikova <jucharia87@gmail.com>
2023-03-12 23:27:25 +0300

    Changed Main again

--- src/main/java/ru/netology/statistic/Main.java
+++ src/main/java/ru/netology/statistic/Main.java
@@ -1,11 +1,2 @@
 package ru.netology.statistic;
 
-public class Main{
-    public static void main(String[] args) {
-        StatisticsService service = new StatisticsService();
-        long[] incomes = {12, 5, -8, 4, 5, -3, 8, 6, 11, 11, 24};
-        long result = service.findMax(incomes);
-        System.out.println(result);
-    }
-
-}
