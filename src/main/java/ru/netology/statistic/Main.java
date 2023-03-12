cd59138fd22e72bc551735bf51ae714c3881018f
Yuliya Mylnikova <jucharia87@gmail.com>
2023-03-12 23:22:45 +0300

    Changed Main

--- src/main/java/ru/netology/statistic/Main.java
+++ src/main/java/ru/netology/statistic/Main.java
@@ -1,7 +1,11 @@
 package ru.netology.statistic;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {12, 5, -8, 4, 5, -3, 8, 6, 11, 11, 24};
+        long result = service.findMax(incomes);
+        System.out.println(result);
     }
+
 }
--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -18,4 +18,28 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMax2() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, -8, 4, 5, -3, 8, 6, 11, 11, 24};
+        long expected = 24;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax3() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {0, 0, 0, 4, 5, 100, 8, 6, 11, 11, 24};
+        long expected = 100;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
